4424defd38c4014970ccc15134e0eda7a9117989
Pedro Sbardelotto <sbardelottopedro97@gmail.com>
2024-09-24 16:25:27 -0300

    segundo commit

--- server.js
+++ server.js
@@ -2,16 +2,25 @@
 import express from 'express'
 
 const app = express() 
+app.use(express.json())
+
+
+
 
 const users = []
 
 //criando uma rota (verbos HTTP) para REQ - RES 
-app.post('/user')
+app.post('/usuarios', (req, res) =>{
+    users.push(req.body)
+
+    res.send('OK aqui deu certo')
+})
 
 
 
 app.get('/usuarios', (req, res) =>{// 1- tipo - metodo http 
-    res.send('Funcionou')
+    res.json(users)
+   
 })
 
 app.listen(3000)
